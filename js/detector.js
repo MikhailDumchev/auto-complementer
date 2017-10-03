bae0a23844bac99090764f0b8d96c217b0b4002f
Sergey Shimansky <sergeyshimansky@MacBook-Air-Sergey.local>
2017-10-03 15:20:20 +0300

    Обусловленное сохранение гео-данных в localStorage (задание Макса); Setter ищет и заполняет поля формы на основании названий свойств объекта geoData

--- js/detector.js
+++ js/detector.js
@@ -1,36 +1,26 @@
 function Detector() {
     var setter = new Object();
-    var city = "";
-    var country = "";
-    var countryEn = "";
-    this.getCity = function() {
-        "use strict";
-        return city;
-    };
-    this.getCountry = function() {
-        "use strict";
-        return country;
-    };
-    this.getCountryEn = function() {
-        "use strict";
-        return countryEn;
-    };
+    var city_rus = "";
+    var country_rus = "";
+    var country_en = "";
     this.getGeoData = function() {
         "use strict";
-        return {"country": country, "city": city, "countryEn": countryEn};
+        return {"country_rus": country_rus, "city_rus": city_rus, "country": country_en};
     };
     //Метод отвечает за определение гео-локации пользователя и установку значений
     //для соответственных полей;
     this.detect = function(form) {
+        "use strict";
         var XHR = new XMLHttpRequest();
         var response = new Object();
         XHR.onreadystatechange = function() {
             if (XHR.readyState === 4) {
                 response = JSON.parse(XHR.responseText);   
                 //Установка значений для гео-полей;
-                countryEn = response.country_code;
-                city = response.city_rus;
-                country = response.country_rus;
+                country_en = response.country_code;
+                city_rus = response.city_rus;
+                country_rus = response.country_rus;
+                //Установка значений для гео-полей формы;
                 try {
                     setter = new Setter();
                     setter.setGeoData(this.getGeoData());
@@ -40,9 +30,21 @@ function Detector() {
                         console.error("Не подключен скрипт 'setter.js';");
                     }
                 }
+                //Сохранение полученных гео-данных в localStorage;
+                localStorage.setItem("fr-user-geo", JSON.stringify(
+                    this.getGeoData()
+                ));
             }
-        };
-        XHR.open("GET", "http://api.2ip.ua/geo.json?key=142dafee11e31629", false);
-        XHR.send();
+        }.bind(this);
+        //Формирование запроса на сервер, если гео-данные ещё не получены;
+        if (!localStorage.getItem("fr-user-geo")) {
+            XHR.open("GET", "http://api.2ip.ua/geo.json?key=142dafee11e31629", false);
+            XHR.send();
+        //Иначе записываем уже имеющиеся;
+        } else {
+            setter = new Setter();
+            setter.setGeoData(JSON.parse(localStorage.getItem("fr-user-geo")));
+            setter.initialize(form);
+        }
     };
 }
--- js/setter.js
+++ js/setter.js
@@ -1,31 +1,25 @@
 function Setter() {
     //Ссылка на HTML-форму;
     var form = new Object();
-    var geoFieldTitle = "geo";
-    var cityFieldTitle = "city";
     //Объект с гео-данными;
     var geoData = new Object();
+    //Метод отвечает за получение гео-данных пользователя;
     this.setGeoData = function(value) {
         "use strict";
         geoData = value;
-    };
-    //Метод отвечает за инициализацию значений в текстовых полях;
-    var setFieldValue = function(title, value) {
-        "use strict";
-        var indicator = false;
-        if (form[title]) {
-            indicator = true;
-            form[title].value = value;
-        } else console.error("К сожалению, поле с таким именем не найдено;");
-        return indicator;
     };
     //Инициализирующий метод;
     this.initialize = function(value) {
         "use strict";
         if (value && value.nodeName === "FORM") {
             form = value;
-            setFieldValue(geoFieldTitle, geoData.country);
-            setFieldValue(cityFieldTitle, geoData.city);
+            //Названия текстовых полей должны быть идентичны названиям свойств
+            //объекта, в котором хранятся гео-данные;
+            for (var key in geoData) {
+                if (form[key] && form[key].hasAttribute("data-geo")) {
+                    form[key].value = geoData[key];
+                }
+            }
         }
     };
 }
