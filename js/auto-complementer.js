b31ebcdab645e6e351927705e67bcc609819e716
Sergey Shimansky <sergeyshimansky@MacBook-Air-Sergey.local>
2017-10-04 16:33:25 +0300

    Переделан механизм определения аббревиатуры страны пользователя

--- js/auto-complementer.js
+++ js/auto-complementer.js
@@ -1,4 +1,8 @@
 function AutoComplementer() {
+    //Название текстового поля, в котором сохраняется аббревиатура текущей страны;
+    var coutryFieldTitle = "country";
+    //Ссылка на форму, которой принадлежит текущее текстовое поле;
+    var form = new Object();
     //Текущая страна пользователя;
     var currentCountry = "";
     //Текстовое поле, к которому добавляется обработчик;
@@ -25,15 +29,16 @@ function AutoComplementer() {
         element.value = "";
     };
     //Метод отвечает за поиск формы, к которой относится данное текстовое поле;
-    var searchForm = function (element) {
+    var searchForm = function () {
         "use strict";
         var indicator = false;
-        while (!indicator && element.nodeName !== "BODY") {
-            if (element.nodeName === "FORM") indicator = true;
+        var additoryVariable = element;
+        while (!indicator && additoryVariable.nodeName !== "BODY") {
+            if (additoryVariable.nodeName === "FORM") indicator = true;
             if (!indicator)
-                element = element.parentNode;
+                additoryVariable = additoryVariable.parentNode;
         }
-        return {"status": indicator, "element": element};
+        return {"status": indicator, "element": additoryVariable};
     };
     //Метод отвечает за определение кода мобильного оператора (в зависимости от
     //страны пользователя);
@@ -63,14 +68,21 @@ function AutoComplementer() {
         XHR.send();
     };
     //Инициализирующий метод;
-    this.create = function (value, geoData) {
+    this.create = function (value) {
         "use strict";
+        var additoryVariable = new Object();
         if (value && value.nodeName === "INPUT") {
             element = value;
-            currentCountry = geoData.countryEn;
             //Очистка поля "Номер телефона" перед началом работы скрипта;
             if (reloadIndictor) reloadPhoneField();
             element.addEventListener("focus", this, false);
+            //Поиск формы, которой принадлежит данное текстовое поле;
+            additoryVariable = searchForm();
+            //Если форма найдена
+            if (additoryVariable.status) {
+                form = additoryVariable.element;
+                if (form.elements[coutryFieldTitle]) currentCountry = form.elements[coutryFieldTitle].value;
+            } else console.error("Форма не найдена;");
         }
     };
     //Обработчик;
